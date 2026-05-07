be2a70ce8f2bd61b85e2096f6fe693fd3d27e1de
williamobrien <william_obrien28@milton.edu>
2026-05-07 10:57:00 -0400

    comit

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -4,18 +4,27 @@ public class BasicGameApp {
     public Card[] deck;
     public Player me;
     public Dealer deal;
+    public static void main(String[] args) {
+    new BasicGameApp();
+    }
+    public BasicGameApp(){
+        System.out.println("Welcome to Blackjack");
+        deck = new Card[52];
+        for (int x=0; x<13; x++){
+            deck[x] = new Card(10, "Hearts", x);
+            deck[x].printInfo();
+        }
+    }
     public void compare(){
 
     }
     public void round(){
 
     }
-public void shuffle(){
+    public void shuffle(){
 
 }
 
-    public static void main(String[] args) {
 
-    }
 
 }
--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,27 @@
-import java.security.PublicKey;
 
 public class Card {
     public String suit;
-   public int valuePoints;
+    public int value;
     public String name;
+
+
+    public Card(int pvalue, String psuit, int pname ) {
+        value= pvalue;
+        suit = psuit;
+        if (pname ==0 ){
+            name = "Ace";
+            value = 11;
+        }
+        else if(pname == 1){
+            name = "2";
+            value= 2;
+        }
+       // name = pname;
+    }
+
+    public void printInfo(){
+        System.out.println("the " + name + " of " + suit + " is worth " + value + " points.");
+
+    }
 
 }
