f2604f1533e63c46779138c0e15fe5366e42371e
williamobrien <william_obrien28@milton.edu>
2026-05-09 21:42:40 -0400

    finished if statements in deck

--- src/Card.java
+++ src/Card.java
@@ -16,6 +16,50 @@ public class Card {
             name = "2";
             value= 2;
         }
+        else if(pname == 2){
+            name = "3";
+            value= 3;
+        }
+        else if(pname == 3){
+            name = "4";
+            value= 4;
+        }
+        else if(pname == 4){
+            name = "5";
+            value= 5;
+        }
+        else if(pname == 5){
+            name = "6";
+            value= 6;
+        }
+        else if(pname == 6){
+            name = "7";
+            value= 7;
+        }
+        else if(pname == 7){
+            name = "8";
+            value= 8;
+        }
+        else if(pname == 8){
+            name = "9";
+            value= 9;
+        }
+        else if(pname == 9){
+            name = "10";
+            value= 10;
+        }
+        else if(pname == 10){
+            name = "Jack";
+            value= 10;
+        }
+        else if(pname == 11){
+            name = "Queen";
+            value= 10;
+        }
+        else if(pname == 12){
+            name = "King";
+            value= 10;
+        }
        // name = pname;
     }
 
