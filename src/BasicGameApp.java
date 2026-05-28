4403b51d740fe785d5a1675daee36d015ebcf614
williamobrien <william_obrien28@milton.edu>
2026-05-28 14:56:08 -0400

    nn

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -5,6 +5,7 @@ public class BasicGameApp {
     public Card[] deck;
     public Player me;
     public Dealer d;
+    public int bet;
 
     public static void main(String[] args) {
         new BasicGameApp();
@@ -27,24 +28,55 @@ public class BasicGameApp {
         Scanner s = new Scanner(System.in);
         System.out.println("what is your name");
         String name = s.nextLine();
+        me = new Player();
+        d = new Dealer();
         boolean playing = true;
         while (playing) {
+            me.hand = new Card[10];
+            d.hand = new Card[10];
+
+            me.cardsInHand = 2;
+            d.cardsInHand = 2;
+
+            me.cardTotal = 0;
+            d.cardTotal = 0;
+
+            me.isBust = false;
+            d.isBust = false;
             shuffle();
             int deckPosition = 6;
             printDeck();
-            me = new Player();
-            d = new Dealer();
             me.hand[0] = deck[0];
             me.hand[1] = deck[1];
             me.calculateTotal();
+
             d.hand[0] = deck[2];
             d.hand[1] = deck[3];
             d.calculateTotal();
 
+            System.out.println("You currently have $" + me.money);
+            if (me.money <= 0){
+                System.out.println("YOU ARE OUT OF MONEY");
+                playing=false;
+            }
+
+                System.out.println("How much would you like to bet?");
+                bet = Integer.parseInt(s.nextLine());
+                while (bet > me.money || bet <= 0) {
+                    System.out.println("INVALID BET");
+                    System.out.println("You currently have " + me.money + " Dollars");
+                    System.out.println("How much would you like to bet?");
+                    bet = Integer.parseInt(s.nextLine());
+                }
+            if (me.cardTotal == 21){
+                System.out.println("BLACKJACK! YOU WIN");
+                me.money += bet;
+            }
 
             me.Name = name;
             me.printInfo();
             d.printFirstCard();
+
             while (me.isBust == false) {
                 System.out.println("Do you want to hit yes or no");
                 String choice = s.nextLine();
@@ -64,7 +96,7 @@ public class BasicGameApp {
             }
 
 
-            if (d.cardTotal < 16) {
+            if (d.cardTotal < 17) {
                 System.out.println("Dealer reveals hidden card");
                 d.printInfo();
             }
@@ -73,19 +105,17 @@ public class BasicGameApp {
                 me.isBust = true;
                 System.out.println("YOU BUSTED");
             }
-            if (d.cardTotal < 16) {
-                d.hand[d.cardsInHand] = deck[4];
+            while (d.cardTotal < 17) {
+                d.hand[d.cardsInHand] = deck[deckPosition++];
                 d.cardsInHand++;
+
                 d.calculateTotal();
-                System.out.println("Dealer Hits");
+
+                System.out.println("Dealer hits");
                 d.printInfo();
             }
-            if (d.cardTotal < 16) {
-                d.hand[d.cardsInHand] = deck[5];
-                d.cardsInHand++;
-                d.calculateTotal();
-                System.out.println("Dealer Hits");
-                d.printInfo();
+            if (d.cardTotal >= 17) {
+                System.out.println("Dealer stands with " + d.cardTotal);
             }
             if (me.cardTotal > 21) {
                 me.isBust = true;
@@ -97,17 +127,25 @@ public class BasicGameApp {
             }
             if (me.isBust && !d.isBust) {
                 System.out.println("DEALER WINS");
+                me.money -=bet;
+
             } else if (d.isBust && !me.isBust) {
-                System.out.println("YOU WINS");
+                System.out.println("YOU WIN");
+                me.money +=bet;
             } else if (d.isBust && me.isBust) {
                 System.out.println("BOTH BUSTED, PUSH");
             } else {
                 if (me.cardTotal > d.cardTotal) {
                     System.out.println("YOU WIN");
+                    me.money +=bet;
+
                 } else if (d.cardTotal > me.cardTotal) {
                     System.out.println("DEALER WINS");
+                    me.money -=bet;
+
                 } else {
                     System.out.println("PUSH");
+
                 }
 
 
